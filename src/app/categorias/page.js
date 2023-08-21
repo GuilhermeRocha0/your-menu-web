2ecfc585f5b3be54bdabc9d48bbb7de584c78580
GuilhermeRocha0 <gui.rocha009@gmail.com>
2023-08-21 10:26:17 -0300

    feat: add navbar and routes

--- src/app/categorias/page.js
+++ src/app/categorias/page.js
@@ -1,12 +1,12 @@
 import NavBar from '@/components/NavBar'
 
-export default function Home() {
+export default function Categorias() {
   return (
     <>
-      <NavBar active={'home'} />
+      <NavBar active={'categorias'} />
 
       <main className="bg-slate-800 m-20 p-12">
-        <h2>Home</h2>
+        <h2>Categorias</h2>
       </main>
     </>
   )
--- src/app/itens/page.js
+++ src/app/itens/page.js
@@ -1,12 +1,12 @@
 import NavBar from '@/components/NavBar'
 
-export default function Home() {
+export default function Itens() {
   return (
     <>
-      <NavBar active={'home'} />
+      <NavBar active={'itens'} />
 
       <main className="bg-slate-800 m-20 p-12">
-        <h2>Home</h2>
+        <h2>Itens</h2>
       </main>
     </>
   )
