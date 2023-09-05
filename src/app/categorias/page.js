c3a542d3ff4bae9bf73015865c2b414a613cea3f
GuilhermeRocha0 <gui.rocha009@gmail.com>
2023-09-04 22:48:15 -0300

    feat: fetch categories

--- src/app/categorias/page.js
+++ src/app/categorias/page.js
@@ -1,12 +1,26 @@
 import NavBar from '@/components/NavBar'
+import CategoryRow from '@/components/CategoryRow'
 
-export default function Categorias() {
+async function getCategories() {
+  const url = 'http://localhost:8080/api/v1/categories'
+  const resp = await fetch(url, { next: { revalidate: 0 } })
+  return resp.json()
+}
+
+export default async function Categorias() {
+  const categories = await getCategories()
+
   return (
     <>
       <NavBar active={'categorias'} />
 
-      <main className="bg-slate-800 m-20 p-12">
-        <h2>Categorias</h2>
+      <main className="bg-slate-800 mt-10 p-9 rounded max-w-xl m-auto">
+        <h2 className="text-slate-100 text-2xl">Categorias</h2>
+        <div id="data">
+          {categories.map(category => (
+            <CategoryRow key={category.id} data={category} />
+          ))}
+        </div>
       </main>
     </>
   )
---
+++ src/components/CategoryRow.jsx
@@ -0,0 +1,19 @@
+import DropMenu from '@/components/DropMenu'
+
+export default function CategoryRow({ data }) {
+  return (
+    <div
+      id="data-row"
+      className="flex items-center justify-between p-2 my-2 rounded cursor-pointer hover:bg-slate-700 group"
+    >
+      <div className="flex gap-2 items-center">
+        <span>{data.name}</span>
+      </div>
+      <div className="flex items-center">
+        <div className="invisible group-hover:visible">
+          <DropMenu />
+        </div>
+      </div>
+    </div>
+  )
+}
